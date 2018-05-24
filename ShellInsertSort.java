670426ad6637456128a54c1f8bde8f49bc6a88dc
wph <491739727@qq.com>
2018-05-24 14:16:20 +0800

    2018:5:24 14:16

--- ShellInsertSort.java
+++ ShellInsertSort.java
@@ -10,6 +10,22 @@ package sort;
 public class ShellInsertSort implements Sort {
     @Override
     public void sort(int[] nums) {
-
+        int length = nums.length;
+        int i,j;
+        int h;
+        int temp;
+        for(h = length/2;h>0;h/=2){
+            for(i=h;i<length;i++){
+                temp = nums[i];
+                for(j = i-h;j>=0;j-=h){
+                    if(temp<nums[j]){
+                        nums[j+h] = nums[j];
+                    }else{
+                        break;
+                    }
+                }
+                nums[j+h] = temp;
+            }
+        }
     }
 }
